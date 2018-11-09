922f48e252b8ea07f6e2adbd2c2b6dce275cc47e
Santiago Lorenzo <santylore4@gmail.com>
2018-11-09 20:29:15 -0300

    Foto con titulo de los videos

--- src/index.js
+++ src/index.js
@@ -1,15 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import YTSearch from 'youtube-api-search'
+import SearchBar from './components/search-bar'
+import VideoList from './components/video-list'
+const API_KEY = 'AIzaSyDbclY_dieZiJ9BrqlrKNJvV-XAnJj9jM8'
 
-import App from './components/app';
-import reducers from './reducers';
+class App extends Component {
+  constructor (props) {
+    super(props)
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+    this.state = { videos: [] }
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos })
+    // this.setState({ videos:videos })
+    })
+  }
+
+  render () {
+    return (
+      <div>
+        <SearchBar />
+        <VideoList videos={this.state.videos} />
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'))
